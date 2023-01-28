8fdc270f341a0e24ba032046580078ebf7226170
Muhammad Arbi Minanda <arbiminanda19@gmail.com>
2023-01-28 17:30:39 +0700

    add env file

---
+++ src/test/java/config/env.java
@@ -0,0 +1,18 @@
+package config;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class env {
+
+    public static AndroidDriver driver;
+    public static DesiredCapabilities capabilities;
+    public static String registeredPhoneNumber = "";
+    public static String registeredPassword = "";
+    public static WebDriverWait wait;
+    public static String baseUrl = "http://127.0.0.1:4723/wd/hub";
+    public static String phoneNumberMoreThan16Char = "081234567890123456";
+
+}
